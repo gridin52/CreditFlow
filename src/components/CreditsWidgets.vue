fc36775ab45f6ce74188ae11c6f0aad2c55d0bc1
gridin52 <ivan_gridin@list.ru>
2025-08-04 22:42:43 +0300

    Добавлен компонент виджета кредита

---
+++ src/components/CreditsWidgets.vue
@@ -0,0 +1,25 @@
+<script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    header: {
+      type: String,
+      required: true
+    },
+    creditValue: {
+      type: String,
+      required: true
+    }
+  })
+</script>
+<template>
+<div class="flex flex-col p-3 rounded-2xl bg-sky-900/50">
+  <!-- Title -->
+  <div class="text-xs text-neutral-400">
+    {{ header }}
+  </div>
+  <div class="font-semibold text-xl">
+    {{ creditValue }}
+  </div>
+</div>
+</template>
