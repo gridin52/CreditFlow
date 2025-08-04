51e915597e1c6b18539fa5f174e2f779677b85dd
gridin52 <ivan_gridin@list.ru>
2025-08-04 22:16:14 +0300

    Добавлен проект с базовым шаблоном

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+  import { RouterView } from 'vue-router';
+  import Header from './components/Header.vue';
+
+</script>
+
+<template>
+  <main class="max-w-[390px] h-screen flex flex-col bg-zinc-950 text-neutral-200">
+    <div>
+      <Header />
+    </div>
+    <div class="flex-1">
+      <RouterView />
+    </div>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+  <div>
+    <!-- Logo -->
+     <div class="text-2xl pt-4 pb-4 pr-2 pl-2 font-bolt">
+      CreditFlow
+     </div>
+  </div>
+</template>
