6263141a80f15685747d6ee7f54b61a93e20650c
gridin52 <ivan_gridin@list.ru>
2025-08-07 21:04:53 +0300

    Виджет кредита в списке переделан на функции

--- src/components/CreditItem.vue
+++ src/components/CreditItem.vue
@@ -1,22 +1,3 @@
-<script setup>
-import { defineProps, computed } from 'vue';
-import { formattedAmount } from '@/utils/formatters';
-
-  const props = defineProps({
-    creditData: {
-      type: Object,
-      required: true
-    }
-  })
-
-  const progress = computed(() => {
-      if (props.creditData.totalPaymentAmount === 0) {
-        return 0
-      }
-      return ((props.creditData.installmentsPaid * props.creditData.monthlyPayment) / props.creditData.totalPaymentAmount * 100)
-    }
-  )
-</script>
 <template>
    <div class="flex flex-col rounded-md pt-4 pb-4 pr-3 pl-3 mb-3 text-neutral-400 bg-neutral-800">
     <!-- Header -->
@@ -39,10 +20,11 @@ import { formattedAmount } from '@/utils/formatters';
     <div class="flex flex-row justify-between items-center mb-3">
       <div>
         <div class=" flex flex-row items-end">
-          <div class="font-bold text-xl text-neutral-50">{{ formattedAmount(creditData.monthlyPayment) }} ₽</div><div class="pl-2 pb-1 text-xs"> / {{ creditData.totalInstallments - creditData.installmentsPaid }} мес.</div>
+          <div class="font-bold text-xl text-neutral-50">{{ formattedAmount(creditData.monthlyPayment) }} ₽</div>
+          <div class="pl-2 pb-1 text-xs"> / {{ remainingInstallments }} мес.</div>
         </div>
         <p class="text-sm">
-          Осталось {{ formattedAmount(creditData.totalPaymentAmount - creditData.installmentsPaid * creditData.monthlyPayment) }} ₽
+          Осталось {{ formattedAmount(remainingAmount) }} ₽
         </p>
         </div>
         <div>
@@ -63,12 +45,53 @@ import { formattedAmount } from '@/utils/formatters';
 
         <div class="flex flex-row justify-between text-xs mt-1">
           <div>
-            {{ parseFloat(((creditData.monthlyPayment * creditData.installmentsPaid) / creditData.totalPaymentAmount * 100).toFixed(0))}}%
+            {{ roundedProgress }}%
           </div>
           <div>
-            {{ creditData.totalPaymentAmount }} ₽
+            {{ formattedAmount(creditData.totalPaymentAmount) }} ₽
           </div>
         </div>
      </div>
    </div>
 </template>
+<script setup>
+import { defineProps, computed } from 'vue';
+import { formattedAmount } from '@/utils/formatters';
+
+  const props = defineProps({
+    creditData: {
+      type: Object,
+      required: true
+    }
+  })
+
+  // Общая уплаченная сумма
+  const amountPaid = computed(() => props.creditData.installmentsPaid * props.creditData.monthlyPayment)
+
+  // Какую сумму осталось платить
+  const remainingAmount = computed(() => props.creditData.totalPaymentAmount - amountPaid.value)
+
+  // Сколько месяцев осталось платить
+  const remainingInstallments = computed(() => props.creditData.totalInstallments - props.creditData.installmentsPaid)
+
+  // Подсчет прогресса оплаты кредита, чтобы двигать Progress Bar
+  const progress = computed(() => {
+      if (props.creditData.totalPaymentAmount === 0) {
+        return 0
+      }
+      return (amountPaid.value / props.creditData.totalPaymentAmount * 100)
+    }
+  )
+  
+  // Перевод выплаченной суммы в проценты от общей суммы кредита
+  const progressPrecentage = computed(() => {
+    if (props.creditData.totalPaymentAmount === 0) {
+      return 0
+    }
+
+    return (amountPaid.value / props.creditData.totalPaymentAmount) * 100
+  })
+
+  // Убирает дроби из процента прогресса
+  const roundedProgress = computed(() => Math.round(progressPrecentage.value))
+</script>
